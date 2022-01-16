8cf35a273f766ccab318f4ec4980dad435162900
Aleksandr Serebro <aleksandrserebroz@gmail.com>
2022-01-16 23:16:21 +0200

    add first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,32 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const imgContainer = document.querySelector('div.gallery');
+const cardMarkup = creteImgCard(galleryItems);
+
+imgContainer.insertAdjacentHTML('beforeend', cardMarkup);
+// imgContainer.addEventListener('click', onImgContainerClick)
+
+function creteImgCard(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+      <a class="gallery__item" href="${original}" onclick="event.preventDefault()">
+        <img class="gallery__image" src="${preview}" alt="Image description" title = "${description}" />
+      </a>
+    </div>`;
+    })
+    .join('');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+});
